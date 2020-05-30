c8efcac7696e130e946a70e3ddeca53de7fcfebc
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 12:19:53 -0500

    Declaring a second float variable

--- Floats_and_IOMANIP.cpp
+++ Floats_and_IOMANIP.cpp
@@ -5,6 +5,11 @@ int main(){
 
 	float x=22.3;	//declaring a float variable
 	cout<<x<<endl;	//printing the float variable
+	
+	
+	float y=23.456;	//declaring a float variable with 3 decimal positions
+	cout<<y<<endl;	//printing the whole number stored in y
+
 	return 0;
 }
 
